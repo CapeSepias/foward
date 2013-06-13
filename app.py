aeb6afcfbb7d6c07f7835d3a8b25cdc46b72af45
Abishek Bhat <bhat.abishek@gmail.com>
2013-06-13 21:54:13 +0530

    Flask App to create XML for Answer_url

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask,request,Response,make_response
+
+import plivo
+
+from config_parser import get_config
+
+configs=get_config()
+
+SIP="sip:abishek130613133829@phone.plivo.com"
+
+MOBILE=919940728522
+
+CALLLER_NAME="plivo"
+
+CALLLER_ID=19512977322
+
+plivo_api=plivo.RestAPI(configs["AUTH_ID"],configs["AUTH_TOKEN"])
+
+app = Flask(__name__)
+
+app.debug=True
+
+@app.route('/forward')
+
+def forward():
+	response=plivo.Response()
+	response.addDial(callerName=CALLLER_NAME).addUser(SIP)
+	response.addDial(callerId=CALLLER_ID).addNumber(MOBILE)
+	response=make_response(response.to_xml())
+	response.headers['Content-Type']='text/xml'
+	
+	return response
+
+
+
+if __name__ == '__main__':
+       app.run(host='0.0.0.0')
+
